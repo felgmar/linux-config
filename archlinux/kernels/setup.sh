df4e75940a843bc336efbd2fd348d0477ddc76aa
Felipe González Martín <15352345-gfelipe099@users.noreply.gitlab.com>
2023-10-26 13:00:20 +0100

    Automatically open customization.cfg with $EDITOR before compiling

--- archlinux/kernels/setup.sh
+++ archlinux/kernels/setup.sh
@@ -6,6 +6,11 @@ then
 
     test ! $(command -v patch) && sudo pacman -Sy --needed --noconfirm "base-devel"
 
+    if test -f "kernels/linux-tkg/customization.cfg"
+    then
+        test $EDITOR && $EDITOR "kernels/linux-tkg/customization.cfg" || exit 1
+    fi
+
     cd "kernels/linux-tkg" && makepkg -cirs --needed && cd "../.."
 else
     cd "kernels/linux-tkg" && git pull && makepkg -cirsf && cd "../.."
