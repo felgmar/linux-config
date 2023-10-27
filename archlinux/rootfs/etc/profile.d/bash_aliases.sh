b94991376fb2786baf541e7b36c689642ba18bfb
Felipe González Martín <15352345-gfelipe099@users.noreply.gitlab.com>
2023-10-27 18:14:02 +0100

    Improve code readability

--- archlinux/rootfs/etc/profile.d/bash_aliases.sh
+++ archlinux/rootfs/etc/profile.d/bash_aliases.sh
@@ -7,9 +7,13 @@ alias ls='ls --color=auto'
 # apt-like pacman wrapper
 pkg()
 {
-    test -z $1 && echo "Usage: `basename $0` [install|update|upgrade|purge|search|clean]" && return 1
+    if test -z $1
+    then
+        echo "Usage: $(basename $0) [install|update|upgrade|purge|search|clean]"
+        return 1
+    fi
 
-    case $1 in
+    case "$1" in
         install) shift; sudo pacman -S --needed "$@"; return $?;;
         remove) shift; sudo pacman -Rsu "$@"; return $?;;
         update) shift; sudo pacman -Sy --needed; return $?;;
@@ -17,8 +21,10 @@ pkg()
         purge) shift; sudo pacman -Rncsd "$@"; return $?;;
         search) shift; pacman -Ss "$@"; return $?;;
         clean) shift; sudo pacman -Rncsd $(pacman -Qqtd); return $?;;
-        *) test ! -z $1 && echo "Unknown command: $1"; return $?;;
+        *) test ! -z $1 && echo "error: $1: unknown command"; return $?;;
     esac
+
+    return 1
 }
 
 # others
