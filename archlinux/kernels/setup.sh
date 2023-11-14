2037b835bfa634449554c32e4cd3433cdd392368
Felipe González Martín <15352345-gfelipe099@users.noreply.gitlab.com>
2023-11-14 08:38:05 +0000

    remove line where it checks if patch is installed

--- archlinux/kernels/setup.sh
+++ archlinux/kernels/setup.sh
@@ -4,8 +4,6 @@ if test ! -d "kernels/linux-tkg"
 then
     git -C "kernels" clone "https://github.com/frogging-family/linux-tkg.git"
 
-    test ! $(command -v patch) && sudo pacman -Sy --needed --noconfirm "base-devel"
-
     if test -f "kernels/linux-tkg/customization.cfg"
     then
         test $EDITOR && $EDITOR "kernels/linux-tkg/customization.cfg" || exit 1
