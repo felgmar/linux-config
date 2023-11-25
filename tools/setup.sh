6201ac4158001d56e802c636b68434d0f39ad9d2
Felipe González Martín <15352345-gfelipe099@users.noreply.gitlab.com>
2023-11-25 12:28:57 +0000

    tools/setup.sh: implement LINUX_DISTRO handling

--- tools/setup.sh
+++ tools/setup.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-if test ! -d "tools/linux-utils"
-then
-    git -C "tools" clone https://gitlab.com/gfelipe099/linux-utils.git
-    sudo sh "tools/linux-utils/archlinux-sbct"
-else
-    git pull
-    sudo sh "tools/linux-utils/archlinux-sbct"
-fi
+case $LINUX_DISTRO in
+    arch|archlinux)
+        if test ! -d "tools/linux-utils"
+        then
+            git -C "tools" clone https://gitlab.com/gfelipe099/linux-utils.git
+            sudo sh "tools/linux-utils/archlinux-sbct"
+        else
+            git pull
+            sudo sh "tools/linux-utils/archlinux-sbct"
+        fi
+    ;;
+esac
