eaa1f584141235a95b4511b0817f9f9913f1c3dc
Felipe González Matín <154735636+gfelipe099@users.noreply.github.com>
2023-12-25 13:28:09 +0000

    Rename yay to paru
    Signed-off-by: Felipe González Matín <154735636+gfelipe099@users.noreply.github.com>

--- packages/setup.sh
+++ packages/setup.sh
@@ -6,10 +6,10 @@ case $LINUX_DISTRO in
         test ! -x "$(command -v ccache)" && sudo pacman -Syu ccache --needed --noconfirm
         test ! -x "$(command -v git)" && sudo pacman -Syu git --needed --noconfirm
 
-        if test ! -x "$(command -v yay)" && test ! -d "packages/yay"
+        if test ! -x "$(command -v paru)" && test ! -d "packages/paru"
         then
-            git -C "packages" clone "https://aur.archlinux.org/yay.git"
-            cd "packages/yay" && makepkg -cCisr && cd "../.."
+            git -C "packages" clone "https://aur.archlinux.org/paru.git"
+            cd "packages/paru" && makepkg -cCisr && cd "../.."
         fi
 
         read -p "Choose a desktop environment [gnome/kde/xfce]: " desktop_environment
@@ -17,18 +17,18 @@ case $LINUX_DISTRO in
         then
             echo "No desktop environment was specified, falling back to 'gnome'."
             desktop_environment="gnome"
-            yay -S --needed $(cat packages/gnome_pkglist)
+            paru -S --needed $(cat packages/gnome_pkglist)
         else
             case $desktop_environment in
-                gnome)      yay -S --needed $(cat packages/gnome_pkglist) && break;;
-                kde|plasma) yay -S --needed $(cat packages/kde_pkglist) && break;;
-                xfce)       yay -S --needed $(cat packages/xfce_pkglist) && break;;
+                gnome)      paru -S --needed $(cat packages/gnome_pkglist) && break;;
+                kde|plasma) paru -S --needed $(cat packages/kde_pkglist) && break;;
+                xfce)       paru -S --needed $(cat packages/xfce_pkglist) && break;;
                 *)          echo "Unknown desktop environment $desktop_environment"; exit 1;;
             esac
         fi
 
-        test -f packages/aur && yay -S --needed $(cat packages/aur) || exit 1
-        test -f packages/pkglist && yay -S --needed $(cat packages/pkglist) || exit 1
+        test -f packages/aur && paru -S --needed $(cat packages/aur) || exit 1
+        test -f packages/pkglist && paru -S --needed $(cat packages/pkglist) || exit 1
         test -f packages/flatpaks && flatpak install --user $(cat packages/flatpaks) || exit 1
 
         if test "$(plymouth-set-default-theme -l | grep archlinux)"
