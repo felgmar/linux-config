72d1a719746588c9c80f4faeea457ef1f3625ec8
Felipe González Martín <15352345-gfelipe099@users.noreply.gitlab.com>
2024-01-17 16:17:13 +0000

    Remove support for any other distro than Arch Linux

--- kernels/setup.sh
+++ kernels/setup.sh
@@ -21,23 +21,8 @@ case "$LINUX_DISTRO" in
         fi
     ;;
     *)
-        if test ! -d "kernels/linux-tkg"
-        then
-            git -C "kernels" clone "https://github.com/frogging-family/linux-tkg.git"
-
-            if test -f "kernels/linux-tkg/customization.cfg"
-            then
-                test $EDITOR && $EDITOR "kernels/linux-tkg/customization.cfg" || exit 1
-            fi
-
-            cd "kernels/linux-tkg" && ./install.sh install && cd "../.."
-        else
-            if test -f "kernels/linux-tkg/customization.cfg"
-            then
-                test $EDITOR && $EDITOR "kernels/linux-tkg/customization.cfg" || exit 1
-            fi
-            cd "kernels/linux-tkg" && git pull && ./install.sh install && cd "../.."
-        fi
+        echo "error: $LINUX_DISTRO: distribution not supported"
+	exit 1
     ;;
 esac
 
