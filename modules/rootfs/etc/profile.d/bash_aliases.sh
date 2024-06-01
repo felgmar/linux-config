805a313dbf9e8d9abba5c8461b93457467c7adb5
Ken Hoo <158048821+mrkenhoo@users.noreply.github.com>
2024-06-01 13:43:48 +0100

    improve the code a bit

--- modules/rootfs/etc/profile.d/bash_aliases.sh
+++ modules/rootfs/etc/profile.d/bash_aliases.sh
@@ -1,13 +1,30 @@
 #!/bin/sh
 
 # detailed file and directory listing
-alias ll='ls -alBih --color=auto'
-alias ls='ls --color=auto'
+alias ll='ls -alih --color=always'
+alias ls='ls --color=always'
+alias rm='trash -v'
+alias cp='cp -i'
+alias mv='mv -i'
+alias edit='$(echo ${EDITOR})'
+
+upgrade_all()
+{
+    paru_bin="$(command -v paru)"
+    paru_args="-Syuu --needed --sudoloop"
+
+     $paru_bin $paru_args
+}
 
 # apt-like pacman wrapper
 pkg()
 {
-    if test -z $1
+    if ! test -x "$(command -v sudo)"
+    then
+        sudo pacman -S --needed base-devel
+    fi
+
+    if test -z "$1"
     then
         echo "Usage: $(basename $0) [install|update|upgrade|purge|search|clean]"
         return 1
@@ -26,3 +43,4 @@ pkg()
 
     return 1
 }
+
