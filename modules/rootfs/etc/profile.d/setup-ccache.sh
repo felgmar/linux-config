053ded618f8c7fc285b40d9543775ac776736b3d
Felipe González Martín <158048821+mrkenhoo@users.noreply.github.com>
2024-06-13 14:37:04 +0100

    fix typo

--- modules/rootfs/etc/profile.d/setup-ccache.sh
+++ modules/rootfs/etc/profile.d/setup-ccache.sh
@@ -4,6 +4,6 @@ if test "$(command -v ccache)"
 then
     ccache --max-size 50G && ccache -o compression=true
 else
-    echo "error: cache was not found and thus compiling will be slower"
+    echo "error: ccache was not found and thus compiling will be slower"
 fi
 
