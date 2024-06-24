33872bf39db03bdaadd4552ef8a1cadbf1bd409a
Felipe González Martín <158048821+mrkenhoo@users.noreply.github.com>
2024-06-24 20:19:40 +0100

    kernels: improve code and it is readability

--- modules/kernels/setup.py
+++ modules/kernels/setup.py
@@ -1,58 +1,60 @@
 #!/usr/bin/env python3
-from os import getcwd, chdir, curdir, path, environ
-from subprocess import run
+import os, getpass, subprocess
 
 class KernelInstaller():
     def __init__(self):
-        self.current_dir: str = getcwd()
+        current_distro_cmd: subprocess.CompletedProcess[str] = subprocess.run("lsb_release -ds", shell=True, universal_newlines=True, capture_output=True, text=True)
+        self.current_distro = current_distro_cmd.stdout.replace("\"", "").removesuffix("\n")
+        self.current_dir: str = os.getcwd()
         self.kernels_directory: str = self.current_dir + "/modules/kernels/"
         self.repo_url: str = ""
+        self.current_user: str = getpass.getuser()
 
     def clone_repo(self, repo_url, repo_directory) -> None:
-        previous_dir: str = curdir
+        previous_dir: str = os.curdir
         command: str = f"git clone {repo_url}"
 
         try:
-            if not path.isdir(self.kernels_directory + repo_directory):
-                chdir(self.kernels_directory)
-                run(command, shell=True)
-                chdir(previous_dir)
+            if not os.path.isdir(self.kernels_directory + repo_directory):
+                os.chdir(self.kernels_directory)
+                subprocess.run(command, shell=True)
+                os.chdir(previous_dir)
             else:
-                chdir(self.kernels_directory)
+                os.chdir(self.kernels_directory)
                 self.update_repo(repo_directory)
-                chdir(previous_dir)
+                os.chdir(previous_dir)
         except Exception:
             raise
 
     def update_repo(self, repo_directory) -> None:
-        previous_dir: str = curdir
+        previous_dir: str = os.curdir
         command: str = f"git pull"
 
         try:
-            chdir(self.kernels_directory + repo_directory)
-            run(command, shell=True)
-            chdir(previous_dir)
+            os.chdir(self.kernels_directory + repo_directory)
+            subprocess.run(command, shell=True)
+            os.chdir(previous_dir)
 
         except Exception:
             raise
 
-    def install_kernel(self, repo_directory: str, distro: str) -> None:
-        if not distro:
+    def install_kernel(self, repo_directory: str, verbose: bool = False) -> None:
+        if not self.current_distro:
             raise ValueError("No distro was specified.")
 
-        previous_dir: str = curdir
+        previous_dir: str = os.curdir
 
-        match distro:
+        match self.current_distro:
             case "Arch Linux":
                 try:
                     self.clone_repo(self.repo_url, repo_directory)
                 except Exception:
                     raise
 
-                if path.isfile("customization.cfg"):
+                if os.path.isfile("customization.cfg"):
                     try:
-                        if environ.get("EDITOR"):
-                            run("$EDITOR customization.cfg", shell=True)
+                        if os.environ.get("EDITOR"):
+                            subprocess.run("$EDITOR customization.cfg", shell=True)
                         else:
                             print("Could not find a valid text editor.")
                             user_text_editor: str = str(input("Specify an alternate text editor: "))
@@ -60,38 +62,42 @@ class KernelInstaller():
                             if user_text_editor:
                                 print(f"The text editor has been set manually to: {user_text_editor}")
 
-                            run(f"{user_text_editor} customization.cfg", shell=True)
+                            subprocess.run(f"{user_text_editor} customization.cfg", shell=True)
                     except Exception:
                         raise
 
-                if path.isfile("PKGBUILD"):
-                    run("makepkg -si", shell=True)
+                if os.path.isfile("PKGBUILD"):
+                    subprocess.run("makepkg -si", shell=True)
 
-                    chdir(previous_dir)
+                    os.chdir(previous_dir)
                 else:
                     self.update_repo(repo_directory)
 
-                    if path.isfile("customization.cfg"):
+                    if os.path.isfile("customization.cfg"):
                         try:
-                            if environ.get("EDITOR"):
-                                run("$EDITOR customization.cfg", shell=True)
+                            if os.environ.get("EDITOR"):
+                                subprocess.run("$EDITOR customization.cfg", shell=True)
                             else:
-                                print("Could not find a valid text editor.")
+                                print("You do not seem to have set a default text editor.")
                                 user_text_editor: str = str(input("Specify an alternate text editor: "))
 
                             if user_text_editor:
-                                print(f"The text editor has been set manually to: {user_text_editor}")
+                                if verbose:
+                                    print(f"The text editor has been set manually to: {user_text_editor}")
 
-                                run(f"{user_text_editor} customization.cfg", shell=True)
+                                subprocess.run(f"{user_text_editor} customization.cfg", shell=True)
+                            else:
+                                if verbose:
+                                    print("No text editor was specified, omitting customization.")
                         except Exception:
                             raise
 
-                    if path.isfile("PKGBUILD"):
-                        run("makepkg -sif", shell=True)
+                    if os.path.isfile("PKGBUILD"):
+                        subprocess.run("makepkg -sif", shell=True)
 
-                    chdir(previous_dir)
+                    os.chdir(previous_dir)
             case _:
-                if distro:
-                    raise NotImplementedError(f"{distro} is not implemented yet")
+                if self.current_distro:
+                    raise NotImplementedError(self.current_distro + " is not implemented yet")
                 else:
                     raise ValueError("No distribution was specified.")
