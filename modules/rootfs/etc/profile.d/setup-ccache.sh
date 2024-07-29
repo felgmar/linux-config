050d9c9f70d6d85edeb66cec769e17cf1cacce5c
Felipe González Martín <158048821+mrkenhoo@users.noreply.github.com>
2024-07-29 15:47:41 +0100

    remove old files

--- modules/rootfs/etc/profile.d/setup-ccache.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if test "$(command -v ccache)"
-then
-    ccache --max-size 50G && ccache -o compression=true
-else
-    echo "error: ccache was not found and thus compiling will be slower"
-fi
-
