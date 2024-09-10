d1d5df275742f656bb60e99bc57bf0bbea9ab6ee
Felipe González Martín <158048821+mrkenhoo@users.noreply.github.com>
2024-09-10 22:38:36 +0100

    fix indentation typo

--- modules/secure_boot.py
+++ modules/secure_boot.py
@@ -68,100 +68,100 @@ class secure_boot_manager():
                 if not os.access(file, os.R_OK):
                     raise FileNotFoundError(file + " is not accessible.")
 
-            if self.current_user != "root":
-                match file:
-                    case "/usr/share/preloader-signed/HashTool.efi":
+                if self.current_user != "root":
+                    match file:
+                        case "/usr/share/preloader-signed/HashTool.efi":
+                            if verbose:
+                                subprocess.run("sudo cp -v {0} /boot/EFI/BOOT".format(file),
+                                    shell=True, universal_newlines=True, text=True)
+                            else:
+                                print("Copying '{0}' to /boot/EFI/BOOT".format(file))
+                                subprocess.run("sudo cp {0} /boot/EFI/BOOT".format(file),
+                                    shell=True, universal_newlines=True, text=True)
+                        case "/usr/share/preloader-signed/PreLoader.efi":
+                            if verbose:
+                                subprocess.run("sudo cp -v {0} /boot/EFI/BOOT/BOOTX64.EFI".format(file),
+                                    shell=True, universal_newlines=True, text=True)
+                            else:
+                                print("Copying '{0}' to /boot/EFI/BOOT/BOOTX64.EFI".format(file))
+                                subprocess.run("sudo cp {0} /boot/EFI/BOOT/BOOTX64.EFI".format(file),
+                                    shell=True, universal_newlines=True, text=True)
+
+                    is_installed: subprocess.CompletedProcess[str] = subprocess.run("sudo bootctl is-installed", shell=True, universal_newlines=True, capture_output=True, text=True)
+                    is_installed_readable: str = str(is_installed).replace("\n", "")
+
+                    if is_installed_readable == "yes":
                         if verbose:
-                            subprocess.run("sudo cp -v {0} /boot/EFI/BOOT".format(file),
+                            print("systemd-boot is already installed")
+                            subprocess.run("sudo bootctl update",
                                 shell=True, universal_newlines=True, text=True)
                         else:
-                            print("Copying '{0}' to /boot/EFI/BOOT".format(file))
-                            subprocess.run("sudo cp {0} /boot/EFI/BOOT".format(file),
+                            print("Updating systemd-boot files...")
+                            subprocess.run("sudo bootctl update",
                                 shell=True, universal_newlines=True, text=True)
-                    case "/usr/share/preloader-signed/PreLoader.efi":
+                    elif is_installed_readable == "no":
                         if verbose:
-                            subprocess.run("sudo cp -v {0} /boot/EFI/BOOT/BOOTX64.EFI".format(file),
-                                shell=True, universal_newlines=True, text=True)
-                        else:
-                            print("Copying '{0}' to /boot/EFI/BOOT/BOOTX64.EFI".format(file))
-                            subprocess.run("sudo cp {0} /boot/EFI/BOOT/BOOTX64.EFI".format(file),
+                            print("systemd-boot is not installed")
+                            subprocess.run("sudo bootctl install --no-variables",
                                 shell=True, universal_newlines=True, text=True)
 
-                is_installed: subprocess.CompletedProcess[str] = subprocess.run("sudo bootctl is-installed", shell=True, universal_newlines=True, capture_output=True, text=True)
+                            subprocess.run("sudo cp -v /boot/EFI/systemd/systemd-bootx64.efi /boot/EFI/BOOT/loader.efi",
+                                            shell=True,universal_newlines=True, text=True)
+                        else:
+                            print("Installing systemd-boot bootloader...")
+                            subprocess.run("sudo bootctl install --no-variables",
+                                            shell=True, universal_newlines=True, text=True)
+                            
+                            print(f"Copying file /boot/EFI/systemd/systemd-bootx64.efi to /boot/EFI/BOOT/loader.efi...")
+                            subprocess.run("sudo cp /boot/EFI/systemd/systemd-bootx64.efi /boot/EFI/BOOT/loader.efi",
+                                            shell=True,universal_newlines=True, text=True)
+                else:
+                    match file:
+                        case "/usr/share/preloader-signed/HashTool.efi":
+                            if verbose:
+                                subprocess.run("cp -v {0} /boot/EFI/BOOT".format(file),
+                                                shell=True, universal_newlines=True, text=True)
+                            else:
+                                print("Copying '{0}' to /boot/EFI/BOOT".format(file))
+                                subprocess.run("cp {0} /boot/EFI/BOOT",
+                                                shell=True, universal_newlines=True, text=True)
+                        case "/usr/share/preloader-signed/PreLoader.efi":
+                            if verbose:
+                                subprocess.run("cp -v {0} /boot/EFI/BOOT/BOOTX64.EFI".format(file),
+                                                shell=True, universal_newlines=True, text=True)
+                            else:
+                                print("Copying '{0}' to /boot/EFI/BOOT/BOOTX64.EFI".format(file))
+                                subprocess.run("cp {0} /boot/EFI/BOOT/BOOTX64.EFI".format(file),
+                                                shell=True, universal_newlines=True, text=True)
+
+                is_installed = subprocess.run("bootctl is-installed",
+                                                shell=True, universal_newlines=True, capture_output=True, text=True)
                 is_installed_readable: str = str(is_installed).replace("\n", "")
 
                 if is_installed_readable == "yes":
                     if verbose:
-                        print("systemd-boot is already installed")
-                        subprocess.run("sudo bootctl update",
+                        print("systemd-boot is installed")
+                        subprocess.run("bootctl update",
                             shell=True, universal_newlines=True, text=True)
                     else:
                         print("Updating systemd-boot files...")
-                        subprocess.run("sudo bootctl update",
+                        subprocess.run("bootctl update",
                             shell=True, universal_newlines=True, text=True)
                 elif is_installed_readable == "no":
                     if verbose:
                         print("systemd-boot is not installed")
-                        subprocess.run("sudo bootctl install --no-variables",
+                        subprocess.run("bootctl install --no-variables",
                             shell=True, universal_newlines=True, text=True)
 
                         subprocess.run("sudo cp -v /boot/EFI/systemd/systemd-bootx64.efi /boot/EFI/BOOT/loader.efi",
                                         shell=True,universal_newlines=True, text=True)
                     else:
                         print("Installing systemd-boot bootloader...")
-                        subprocess.run("sudo bootctl install --no-variables",
+                        subprocess.run("bootctl install --no-variables",
                                         shell=True, universal_newlines=True, text=True)
-                        
+
                         print(f"Copying file /boot/EFI/systemd/systemd-bootx64.efi to /boot/EFI/BOOT/loader.efi...")
                         subprocess.run("sudo cp /boot/EFI/systemd/systemd-bootx64.efi /boot/EFI/BOOT/loader.efi",
                                         shell=True,universal_newlines=True, text=True)
-            else:
-                match file:
-                    case "/usr/share/preloader-signed/HashTool.efi":
-                        if verbose:
-                            subprocess.run("cp -v {0} /boot/EFI/BOOT".format(file),
-                                            shell=True, universal_newlines=True, text=True)
-                        else:
-                            print("Copying '{0}' to /boot/EFI/BOOT".format(file))
-                            subprocess.run("cp {0} /boot/EFI/BOOT",
-                                            shell=True, universal_newlines=True, text=True)
-                    case "/usr/share/preloader-signed/PreLoader.efi":
-                        if verbose:
-                            subprocess.run("cp -v {0} /boot/EFI/BOOT/BOOTX64.EFI".format(file),
-                                            shell=True, universal_newlines=True, text=True)
-                        else:
-                            print("Copying '{0}' to /boot/EFI/BOOT/BOOTX64.EFI".format(file))
-                            subprocess.run("cp {0} /boot/EFI/BOOT/BOOTX64.EFI".format(file),
-                                            shell=True, universal_newlines=True, text=True)
-
-            is_installed = subprocess.run("bootctl is-installed",
-                                            shell=True, universal_newlines=True, capture_output=True, text=True)
-            is_installed_readable: str = str(is_installed).replace("\n", "")
-
-            if is_installed_readable == "yes":
-                if verbose:
-                    print("systemd-boot is installed")
-                    subprocess.run("bootctl update",
-                        shell=True, universal_newlines=True, text=True)
-                else:
-                    print("Updating systemd-boot files...")
-                    subprocess.run("bootctl update",
-                        shell=True, universal_newlines=True, text=True)
-            elif is_installed_readable == "no":
-                if verbose:
-                    print("systemd-boot is not installed")
-                    subprocess.run("bootctl install --no-variables",
-                        shell=True, universal_newlines=True, text=True)
-
-                    subprocess.run("sudo cp -v /boot/EFI/systemd/systemd-bootx64.efi /boot/EFI/BOOT/loader.efi",
-                                    shell=True,universal_newlines=True, text=True)
-                else:
-                    print("Installing systemd-boot bootloader...")
-                    subprocess.run("bootctl install --no-variables",
-                                    shell=True, universal_newlines=True, text=True)
-                    
-                    print(f"Copying file /boot/EFI/systemd/systemd-bootx64.efi to /boot/EFI/BOOT/loader.efi...")
-                    subprocess.run("sudo cp /boot/EFI/systemd/systemd-bootx64.efi /boot/EFI/BOOT/loader.efi",
-                                    shell=True,universal_newlines=True, text=True)
         except:
             raise
