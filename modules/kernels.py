5eae250e788c38896b478e97cca04c82bc2e99a1
Felipe González Martín <158048821+felgmar@users.noreply.github.com>
2024-11-20 13:55:16 +0000

    modules: major refactor to kernels.py

--- modules/kernels.py
+++ modules/kernels.py
@@ -1,89 +1,68 @@
 #!/usr/bin/env python3
 import os, getpass, subprocess
+from modules.repository import repository_manager
 
 class kernel_manager():
     def __init__(self):
-        self.repo_url: str = ""
-        self.repo_dir: str = ""
-
         self.current_distro_cmd: subprocess.CompletedProcess[str] = \
-            subprocess.run("lsb_release -ds", shell=True, universal_newlines=True,
-                           capture_output=True, text=True)
+            subprocess.run("lsb_release -ds", shell=True, universal_newlines=True, capture_output=True, text=True)
 
         self.current_user: str = getpass.getuser()
         self.current_distro: str = self.current_distro_cmd.stdout.replace("\"", "").removesuffix("\n")
 
         self.current_dir: str = os.getcwd()
-        self.kernels_directory: str = self.current_dir + "/modules/kernels/"
-
-    def update_repo(self) -> None:
-        previous_dir: str = os.curdir
-        command: str = f"git pull"
-
-        try:
-            if os.path.isdir(self.kernels_directory + self.repo_dir):
-                os.chdir(self.kernels_directory + self.repo_dir)
-                subprocess.run(command, shell=True)
-                os.chdir(previous_dir)
-        except:
-            raise IOError(f"{self.kernels_directory + self.repo_dir}: such directory does not exist.")
-
-    def clone_repo(self) -> None:
-        previous_dir: str = os.curdir
-        command: str = f"git clone {self.repo_url} {self.repo_dir}"
-
-        try:
-            if not os.path.isdir(self.kernels_directory + self.repo_dir):
-                os.chdir(self.kernels_directory)
-                subprocess.run(command, shell=True)
-                os.chdir(previous_dir)
-            else:
-                self.update_repo()
-        except Exception:
-            raise
 
     def install_kernel(self, verbose: bool = False) -> None:
-        previous_dir: str = os.curdir
+        previous_dir: str = self.current_dir
+
+        rm = repository_manager()
+        rm.repo_url = "https://github.com/frogging-family/linux-tkg.git"
+        rm.repo_dir = "linux-tkg"
 
         if not self.current_distro:
             raise ValueError("No distro was specified.")
 
-        if not os.path.isdir(self.kernels_directory):
-            os.makedirs(self.kernels_directory)
-
-        try:
-            self.clone_repo()
-        except:
-            raise
-
         match self.current_distro:
             case "Arch Linux":
-                if os.path.isdir(self.kernels_directory):
-                    if os.path.isfile("customization.cfg"):
-                        try:
-                            if os.environ.get("EDITOR"):
-                                subprocess.run("$EDITOR customization.cfg", shell=True)
-                            else:
-                                print("You do not seem to have set a default text editor.")
-                                user_text_editor: str = str(input("Specify an alternate text editor: "))
+                try:
+                    rm.clone_repo()
+                except Exception:
+                    raise
 
-                                if user_text_editor:
-                                    if verbose:
-                                        print(f"The text editor has been set manually to: {user_text_editor}")
+                if not os.path.exists(os.path.join(rm.repositories_dir, rm.repo_dir)):
+                    raise IOError(os.path.join(rm.repositories_dir, rm.repo_dir), ": directory not found")
+                
+                if not os.path.isfile(os.path.join(rm.repositories_dir, rm.repo_dir, "customization.cfg")):
+                    raise FileNotFoundError("customization.cfg: file not found")
+                
+                try:
+                    os.chdir(os.path.join(rm.repositories_dir, rm.repo_dir))
+                except Exception:
+                    raise
 
-                                    subprocess.run(f"{user_text_editor} customization.cfg", shell=True)
-                                else:
-                                    if verbose:
-                                        print("No text editor was specified, omitting customization.")
-                        except Exception:
-                            raise
+                if os.environ.get("EDITOR"):
+                    subprocess.run("$EDITOR customization.cfg", shell=True)
+                else:
+                    print("You do not seem to have set a default text editor.\n" + 
+                            "to edit the customization.cfg file.")
+                    try:
+                        user_text_editor: str = input("Specify an alternate text editor: ")
+                    except EOFError:
+                        raise
 
-                        if os.path.isfile("PKGBUILD"):
-                            subprocess.run("makepkg -sirf", shell=True)
+                    if user_text_editor:
+                        if verbose:
+                            print(f"The text editor has been set manually to: {user_text_editor}")
 
-                            os.chdir(previous_dir)
+                        subprocess.run(f"{user_text_editor} customization.cfg", shell=True)
                     else:
-                        raise FileNotFoundError("customization.cfg: file not found")
+                        if verbose:
+                            print("No text editor was specified, omitting customization.")
+
+                if os.path.isfile("PKGBUILD"):
+                    subprocess.run("makepkg -sirf", shell=True)
+
+                    os.chdir(previous_dir)
             case _:
                 if self.current_distro:
                     raise NotImplementedError(self.current_distro + " is not implemented yet.")
