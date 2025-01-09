0004404432027a820bc7997c5e3212df5d1c6b89
Felipe González Martín <158048821+felgmar@users.noreply.github.com>
2025-01-09 17:39:11 +0000

    modules: improve and fix secure_boot

--- modules/secure_boot.py
+++ modules/secure_boot.py
@@ -5,6 +5,7 @@ Module containing the SecureBootManager class.
 """
 
 import getpass
+from multiprocessing import Value
 import subprocess
 import os
 
@@ -25,15 +26,43 @@ class SecureBootManager():
             "preloader-signed"
         ]
 
-        pm = PackageManager()
-        pm_bin = pm.get_package_manager(override_package_manager=True)
+        PM = PackageManager()
+        PM_BIN = PM.get_package_manager(override_package_manager=True)
 
         if verbose:
-            print("The package manager has been set to:", pm_bin)
+            print("The package manager has been set to:", PM_BIN)
 
-        pm.install_packages(pm_bin, custom_pkglist=pkglist)
+        PM.install_packages(PM_BIN, custom_pkglist=pkglist)
+    
+    def _check_file_access(self, file: str) -> None:
+        if not os.access(file, os.R_OK):
+            raise FileNotFoundError(file + " is not accessible.")
 
-    def backup_boot_files(self, verbose: bool = False) -> None:
+    def _copy_file(self, source: str, destination: str, verbose: bool = False):
+        command: str = ""
+
+        if self.current_user == "root":
+            if verbose:
+                command = f"cp -v {source} {destination}"
+            else:
+                command = f"cp {source} {destination}"
+        else:
+            if verbose:
+                command = f"sudo cp -v {source} {destination}"
+            else:
+                command = f"sudo cp {source} {destination}"
+
+        if command == "":
+            raise ValueError(command)
+
+        try:
+            subprocess.run(command, shell=True,
+                           universal_newlines=True,
+                           check=True, text=True)
+        except Exception as e:
+            raise e
+
+    def backup_boot_files(self) -> None:
         """
         Backs up the boot files.
         """
@@ -45,76 +74,32 @@ class SecureBootManager():
             try:
                 match file:
                     case "/boot/EFI/BOOT/BOOTX64.EFI":
-                        if self.current_user != "root":
-                            if verbose:
-                                subprocess.run(f"sudo cp -v {file} {file}.backup",
-                                                shell=True, universal_newlines=True,
-                                                check=True, text=True)
-                            else:
-                                print(f"Copying '{file}' to '{file}.backup'")
-                                subprocess.run(f"sudo cp '{file}' {file}.backup",
-                                                shell=True, universal_newlines=True,
-                                                check=True, text=True)
-                        else:
-                            if verbose:
-                                subprocess.run(f"cp -v '{file}' {file}.backup",
-                                                shell=True, universal_newlines=True,
-                                                check=True, text=True)
-                            else:
-                                print(f"Copying '{file}' to '{file}.backup'")
-                                subprocess.run(f"cp '{file}' '{file}.backup'",
-                                                shell=True, universal_newlines=True,
-                                                check=True, text=True)
+                        self._check_file_access(file)
+                        self._copy_file(file, f"{file}.backup")
                     case _:
-                        return
+                        raise FileNotFoundError(file)
             except Exception as e:
                 raise e
-
-    def _check_file_access(self, file: str) -> None:
-        if not os.access(file, os.R_OK):
-            raise FileNotFoundError(file + " is not accessible.")
-
-    def _copy_file(self, file: str, verbose: bool) -> None:
-        if self.current_user != "root":
-            if file == "/usr/share/preloader-signed/HashTool.efi":
-                self._copy_hash_tool(file, verbose)
-            elif file == "/usr/share/preloader-signed/PreLoader.efi":
-                self._copy_preloader(file, verbose)
-
-    def _copy_hash_tool(self, file: str, verbose: bool) -> None:
-        if verbose:
-            subprocess.run(f"sudo cp -v '{file}' '/boot/EFI/BOOT'",
-                           shell=True, universal_newlines=True,
-                           check=True, text=True)
-        else:
-            print(f"Copying '{file}' to '/boot/EFI/BOOT'")
-            subprocess.run(f"sudo cp '{file}' '/boot/EFI/BOOT'",
-                           shell=True, universal_newlines=True,
-                           check=True, text=True)
-
-    def _copy_preloader(self, file: str, verbose: bool):
-        if verbose:
-            subprocess.run(f"sudo cp -v '{file}' '/boot/EFI/BOOT'",
-                           shell=True, universal_newlines=True,
-                           check=True, text=True)
-        else:
-            print(f"Copying '{file}' to '/boot/EFI/BOOT'")
-            subprocess.run(f"sudo cp '{file}' '/boot/EFI/BOOT'",
-                           shell=True, universal_newlines=True,
-                           check=True, text=True)
 
     def install_shim(self, verbose: bool = False):
         """
         Installs the shim packages.
         """
-        try:
-            preloader_files: list[str] = [
-                "/usr/share/preloader-signed/HashTool.efi",
-                "/usr/share/preloader-signed/PreLoader.efi",
-            ]
+        preloader_files: list[str] = [
+            "/usr/share/preloader-signed/HashTool.efi",
+            "/usr/share/preloader-signed/PreLoader.efi",
+        ]
 
-            for file in preloader_files:
-                self._check_file_access(file)
-                self._copy_file(file, verbose)
-        except Exception as e:
-            raise e
+        for file in preloader_files:
+            try:
+                match file:
+                    case "/usr/share/preloader-signed/HashTool.efi":
+                        self._check_file_access(file)
+                        self._copy_file(file, "/boot/EFI/BOOT")
+                    case "/usr/share/preloader-signed/PreLoader.efi":
+                        self._check_file_access(file)
+                        self._copy_file(file, "/boot/EFI/BOOT/loader.efi")
+                    case _:
+                        raise FileNotFoundError(file)
+            except Exception as e:
+                raise e
