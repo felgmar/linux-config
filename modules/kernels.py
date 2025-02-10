e34baa478733056b4de04306a6052c1850e34147
Felipe González Martín <158048821+felgmar@users.noreply.github.com>
2025-02-10 13:10:19 +0000

    modules/kernels: do not for current distro anymore

--- modules/kernels.py
+++ modules/kernels.py
@@ -6,6 +6,7 @@ Module containing the KernelManager class.
 
 import os
 import getpass
+import shutil
 import subprocess
 
 from modules.repository import RepositoryManager
@@ -15,13 +16,7 @@ class KernelManager():
     Manages the installation of custom kernels.
     """
     def __init__(self, kernel_url: str, kernel_dir: str):
-        self.current_distro_cmd: subprocess.CompletedProcess[str] = \
-            subprocess.run("lsb_release -ds", shell=True, universal_newlines=True,
-                           capture_output=True, check=True, text=True)
-
         self.current_user: str = getpass.getuser()
-        self.current_distro: str = \
-            self.current_distro_cmd.stdout.replace("\"", "").removesuffix("\n")
 
         self.current_dir: str = os.getcwd()
 
@@ -55,49 +50,45 @@ class KernelManager():
         """
         previous_dir: str = self.current_dir
 
-        if not self.current_distro:
-            raise ValueError("No distro was specified.")
+        if not os.path.isdir(os.path.join(self.repo_manager.repositories_dir,
+                                            self.repo_manager.repo_dir)):
+            try:
+                self.clone_kernel()
+            except Exception as e:
+                raise e
+        else:
+            try:
+                self.update_kernel()
+            except Exception as e:
+                raise e
 
-        match self.current_distro:
-            case "Arch Linux":
-                if not os.path.isdir(os.path.join(self.repo_manager.repositories_dir,
-                                                  self.repo_manager.repo_dir)):
-                    try:
-                        self.clone_kernel()
-                    except Exception as e:
-                        raise e
-                else:
-                    try:
-                        self.update_kernel()
-                    except Exception as e:
-                        raise e
+        os.chdir(os.path.join(self.repo_manager.repositories_dir,
+                                self.repo_manager.repo_dir))
 
-                os.chdir(os.path.join(self.repo_manager.repositories_dir,
-                                      self.repo_manager.repo_dir))
+        if os.environ.get("EDITOR"):
+            subprocess.run("$EDITOR customization.cfg", shell=True, check=True)
+        else:
+            print("You do not seem to have set a default text editor.\n" + \
+                    "to edit the customization.cfg file.")
+            try:
+                user_text_editor: str = input("Specify an alternate text editor: ")
+            except EOFError as e:
+                raise e
 
-                if os.environ.get("EDITOR"):
-                    subprocess.run("$EDITOR customization.cfg", shell=True, check=True)
-                else:
-                    print("You do not seem to have set a default text editor.\n" + \
-                          "to edit the customization.cfg file.")
-                    try:
-                        user_text_editor: str = input("Specify an alternate text editor: ")
-                    except EOFError as e:
-                        raise e
+            if user_text_editor:
+                if verbose:
+                    print(f"The text editor has been set manually to: {user_text_editor}")
 
-                    if user_text_editor:
-                        if verbose:
-                            print(f"The text editor has been set manually to: {user_text_editor}")
+                if not shutil.which(user_text_editor):
+                    raise FileNotFoundError(f"Could not find {user_text_editor} on your system.")
 
-                        subprocess.run(f"{user_text_editor} customization.cfg",
-                                       shell=True, check=True)
-                    else:
-                        if verbose:
-                            print("No text editor was specified, omitting customization.")
+                subprocess.run(f"{user_text_editor} customization.cfg",
+                                shell=True, check=True)
+            else:
+                if verbose:
+                    print("No text editor was specified, omitting customization.")
 
-                if os.path.isfile("PKGBUILD"):
-                    subprocess.run("makepkg -sirf", shell=True, check=True)
+        if os.path.isfile("PKGBUILD"):
+            subprocess.run("makepkg -sirf", shell=True, check=True)
 
-                os.chdir(previous_dir)
-            case _:
-                raise NotImplementedError(self.current_distro + " is not implemented yet.")
+        os.chdir(previous_dir)
