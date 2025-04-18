bf641f954359d8b79abca31be8c2170895c62ef8
Felipe González Martín <158048821+felgmar@users.noreply.github.com>
2025-04-18 14:46:30 +0100

    delete packages used from elsewhere

--- modules/packages.py
+++ modules/packages.py
@@ -169,25 +169,15 @@ class PackageManager():
             case "Arch":
                 aur = [
                     "archlinux-artwork",
-                    "bottles",
-                    "duckstation-git",
-                    "ckb-next-git",
-                    "github-desktop",
                     "lib32-mangohud-git",
                     "mangohud-git",
                     "minq-ananicy-git",
                     "mkinitcpio-firmware",
-                    "pcsx2-git",
                     "plymouth-theme-archlinux",
                     "preloader-signed",
-                    "protonup-rs",
                     "rate-mirrors",
-                    "rpcs3-git",
-                    "spotify",
-                    "ttf-meslo-nerd-font-powerlevel10k",
                     "ventoy-bin",
                     "visual-studio-code-bin",
-                    "zsh-theme-powerlevel10k-git"
                 ]
 
                 gnome = [
