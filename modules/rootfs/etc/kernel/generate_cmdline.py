65a7602094aef2333d659614a8b2c633bf5201d7
Felipe González Martín <158048821+felgmar@users.noreply.github.com>
2025-04-30 16:56:14 +0100

    add new tool to generate a cmdline

---
+++ modules/rootfs/etc/kernel/generate_cmdline.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+"""
+This module helps with the creation of generating a cmdline for the Linux kernel.
+"""
+
+import os
+import re
+import shutil
+import sys
+import subprocess
+
+CURRENT_PLATFORM: str = sys.platform.lower()
+CURRENT_USER: str = os.getlogin()
+CMDLINE_FILE: str = "/etc/kernel/cmdline"
+
+CURRENT_DIR: str = os.getcwd()
+
+def _generate_cmdline(partition: str) -> str:
+    """
+    Generate the kernel command line for the current system.
+    """
+    assert partition is not None, "A root partition is required."
+
+    args: list[str] = ["blkid", "-s", "PARTUUID", "-o", "value", partition]
+    partition_partuuid: str = \
+        subprocess.run(args, capture_output=True, check=False, text=True).stdout.strip()
+    cmdline_path: str = os.path.join(CURRENT_DIR, "modules/rootfs/etc/kernel/cmdline_no_encryption")
+
+    with open(cmdline_path, "r", encoding="UTF-8") as file:
+        new_cmdline: str = re.sub(r"<.*>", partition_partuuid, file.read().strip())
+
+    return new_cmdline
+
+def _write_cmdline(file: str) -> None:
+    """
+    Write the generated command line to the kernel command line file.
+    """
+    assert os.path.exists(CMDLINE_FILE[:-8])
+
+    assert os.access(CMDLINE_FILE, os.W_OK), f"{CMDLINE_FILE}: cannot write to file"
+
+    if os.path.isfile(CMDLINE_FILE):
+        try:
+            shutil.copyfile(CMDLINE_FILE, CMDLINE_FILE + ".backup")
+        except Exception as e:
+            raise e
+
+    with open("/etc/kernel/cmdline", "w", encoding="UTF-8") as item:
+        item.write(file)
+
+if __name__ == "__main__":
+    assert CURRENT_PLATFORM == "linux", "This script only runs on Linux"
+    assert CURRENT_USER != "root", "This script must be run as root"
+
+    root_partition: str = str(input("Enter the root partition (e.g., /dev/sda1): ")).strip()
+
+    try:
+        cmdline = _generate_cmdline(root_partition)
+    except Exception as e:
+        raise e
+
+    try:
+        _write_cmdline(cmdline)
+    except Exception as e:
+        raise e
