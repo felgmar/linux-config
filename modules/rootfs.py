b47250ab0fd324c806f7eb1659a45a8bd616ed4d
Felipe González Martín <158048821+felgmar@users.noreply.github.com>
2025-06-14 10:28:57 +0100

    Refactor "__create_list" with unused functions and fix typos

--- modules/rootfs.py
+++ modules/rootfs.py
@@ -22,10 +22,10 @@ class RootFSManager():
         self.current_user: str = getpass.getuser()
 
         if not override_permissions:
-            if not self._is_admin():
+            if not self.__is_admin():
                 raise PermissionError(f"Insufficient permissions for the class {self.__class__.__name__}")
 
-    def _is_admin(self) -> bool:
+    def __is_admin(self) -> bool:
         """
         Checks if the current user is an administrator.
         """
@@ -33,7 +33,7 @@ class RootFSManager():
             return True
         return False
 
-    def _is_directory(self, directory: str) -> bool:
+    def __is_directory(self, directory: str) -> bool:
         """
         Checks if a directory exists.
         """
@@ -44,7 +44,7 @@ class RootFSManager():
         except Exception as e:
             raise e
 
-    def _is_file(self, file: str) -> bool:
+    def __is_file(self, file: str) -> bool:
         """
         Checks if a file exists.
         """
@@ -55,9 +55,9 @@ class RootFSManager():
         except Exception as e:
             raise e
 
-    def _create_list(self, from_directory: str, verbose: bool = False) -> list[str]:
+    def __create_list(self, from_directory: str, verbose: bool = False) -> list[str]:
         """
-        Creates a list from the specified directory type.
+        Creates a list from their specified directory type.
         """
         file_list: list[str] = []
 
@@ -72,34 +72,37 @@ class RootFSManager():
                     raise e
         return file_list
 
-    def _copy_files(self, source: str, destination: str,
+    def __copy_files(self, source: str, destination: str,
                     verbose: bool = False) -> None:
         """
-        Copies the files to the respective directories.
+        Copies the files to their respective directories.
         """
         if not source:
             raise ValueError("No source was provided.")
 
-        if not self._is_directory(destination):
+        if not self.__is_directory(destination):
             raise FileNotFoundError(f"{destination} does not exist.")
 
-        for root, _, files in os.walk(source):
-            for file in files:
-                file_path: str = os.path.relpath(os.path.join(root, file))
-                file_relative_path: str = os.path.relpath(file_path, source)
-                destination_path: str = os.path.join(destination, file_relative_path)
-                is_dir: bool = os.path.isdir(os.path.dirname(destination_path))
+        file_list: list[str] = self.__create_list(source, verbose)
 
-                try:
-                    if is_dir:
-                        if verbose:
-                            print(f"[VERBOSE] Copying '{file_path}' to '{destination_path}'...")
-                        shutil.copyfile(file_path, destination_path)
-                    else:
-                        print(f"[!] Skipping directory {os.path.dirname(destination_path)}, since it does not exist.")
-                        break
-                except Exception as e:
-                    raise e
+        for file in file_list:
+            file_path: str = os.path.relpath(file)
+            file_relative_path: str = os.path.relpath(file_path, source)
+            destination_path: str = os.path.join(destination, file_relative_path)
+            is_dir: bool = os.path.isdir(os.path.dirname(destination_path))
+
+            try:
+                assert self.__is_file(file_path), f"{file_path} is not a file."
+
+                if is_dir:
+                    if verbose:
+                        print(f"[VERBOSE] Copying '{file_path}' to '{destination_path}'...")
+                    shutil.copyfile(file_path, destination_path)
+                else:
+                    print(f"[!] Skipping directory {os.path.dirname(destination_path)}, since it does not exist.")
+                    break
+            except Exception as e:
+                raise e
 
     def install_files(self, verbose: bool = False) -> None:
         """
@@ -113,12 +116,12 @@ class RootFSManager():
         }
 
         try:
-            self._copy_files(self.local_dirs["boot_dir"], paths["boot"], verbose)
-            self._copy_files(self.local_dirs["etc_dir"], paths["etc"], verbose)
+            self.__copy_files(self.local_dirs["boot_dir"], paths["boot"], verbose)
+            self.__copy_files(self.local_dirs["etc_dir"], paths["etc"], verbose)
             if not self.current_user == "root":
-                self._copy_files(self.local_dirs["home_dir"], paths["home"], verbose)
+                self.__copy_files(self.local_dirs["home_dir"], paths["home"], verbose)
             else:
                 print("[!] Not copying home files for the root user.")
-            self._copy_files(self.local_dirs["usr_dir"], paths["usr"], verbose)
+            self.__copy_files(self.local_dirs["usr_dir"], paths["usr"], verbose)
         except Exception as e:
             raise e
