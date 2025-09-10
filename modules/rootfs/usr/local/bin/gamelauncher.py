594b5de6b04c6e22a5eb3c932766fafdc1217bcb
Felipe González Martín <158048821+felgmar@users.noreply.github.com>
2025-09-10 09:08:12 +0100

    modules/rootfs: major rewriting to gamelauncher
    - Add private variables and new boolean checks.
    - Add new functions to modify some of them: set_display_resolution, set_refresh_rate.
    - Rearranged command_line appends.
    - Implement or use these changes on the main function and also print them out.

--- modules/rootfs/usr/local/bin/gamelauncher.py
+++ modules/rootfs/usr/local/bin/gamelauncher.py
@@ -11,22 +11,63 @@ class GameLauncher():
     """
     def __init__(self, args: list[str]) -> None:
         self.args: list[str] = args
-        self.is_gamescope_available: bool = bool(shutil.which("gamescope"))
-        self.is_mangohud_available: bool = bool(shutil.which("mangohud"))
-        self.is_gamemoderun_available: bool = bool(shutil.which("gamemoderun"))
-        self.is_mangohud_dlsym_enabled: bool = False
 
-        for arg in args:
-            if arg == "AppId=255710":
-                self.is_mangohud_dlsym_enabled = True
-                #print("found {}!".format(arg))
+        self.app_id: str = ""
+        self.refresh_rate: int = 0
+        self.fullscreen_mode: bool = False
+        self.always_grab_cursor: bool = True
 
         self.gamescope_path: str = str(shutil.which("gamescope"))
+        self.mangoapp_path: str = str(shutil.which("mangoapp"))
         self.mangohud_path: str = str(shutil.which("mangohud"))
         self.gamemoderun_path: str = str(shutil.which("gamemoderun"))
 
+        self.is_wayland_available: bool = os.environ.get("XDG_SESSION_TYPE") == "wayland"
+        self.is_gamescope_available: bool = bool(shutil.which("gamescope"))
+        self.is_mangoapp_available: bool = bool(shutil.which("mangoapp"))
+        self.is_mangohud_available: bool = bool(shutil.which("mangohud"))
+        self.is_gamemoderun_available: bool = bool(shutil.which("gamemoderun"))
+        self.is_mangohud_dlsym_available: bool = subprocess.run("mangohud --dlsym",
+                                                                shell=True,
+                                                                stdout=subprocess.PIPE,
+                                                                stderr=subprocess.PIPE).returncode == 0
+
+
+        for arg in args:
+            if arg == "AppId=255710":
+                self.app_id = "255710"
+                print("[INFO] found {}!".format(arg))
+                break
+
         self.CURRENT_USER: str = os.getlogin()
         self.CURRENT_PLATFORM: str = sys.platform.lower()
+
+    def set_display_resolution(self, width: int, height: int) -> dict[str, int]:
+        """
+        Sets the display resolution using gamescope.
+        Args:
+            width (int): The desired width.
+            height (int): The desired height.
+        Returns:
+            Dictionary containing the width and hight
+        """
+        display_resolution: dict[str, int] = {}
+
+        if not self.is_gamescope_available:
+            raise RuntimeError("gamescope is not available on this system.")
+
+        display_resolution["width"] = width
+        display_resolution["height"] = height
+
+        return display_resolution
+
+    def set_refresh_rate(self, new_refresh_rate: int) -> None:
+        """
+        Set the refresh rate to be used by gamescope.
+        Args:
+            refresh_rate: int
+        """
+        self.refresh_rate = new_refresh_rate
 
     def run(self) -> int:
         """
@@ -34,33 +75,63 @@ class GameLauncher():
         Returns:
             int: The exit code of the game process.
         """
-        if self.CURRENT_PLATFORM != "linux":
-            raise RuntimeError("Your platform {} is not compatible.",
-                               self.CURRENT_PLATFORM)
+        if not self.CURRENT_PLATFORM == "linux":
+            raise RuntimeError(f"Your platform '{self.CURRENT_PLATFORM}' is not supported.")
 
         if self.CURRENT_USER == "root":
             raise PermissionError("Do not run this script as root.")
+
+        try:
+            display = self.set_display_resolution(1920, 1080)
+        except Exception as e:
+            raise e
+
+        try:
+            self.set_refresh_rate(75)
+        except Exception as e:
+            raise e
 
         command_line: list[str] = []
 
         if self.is_gamescope_available:
             command_line.append(self.gamescope_path)
-            command_line.append("--")
+            command_line.append(f"-W {display["width"]}")
+            command_line.append(f"-H {display["height"]}")
+            if not self.refresh_rate == 0:
+                command_line.append(f"-r {self.refresh_rate}")
+            if self.is_wayland_available:
+                command_line.append("--expose-wayland")
+            if self.fullscreen_mode:
+                command_line.append("--fullscreen")
+            if self.is_mangohud_available and self.is_mangoapp_available:
+                command_line.append("--mangoapp")
         else:
             if self.is_mangohud_available:
                 command_line.append(self.mangohud_path)
-            if self.is_mangohud_dlsym_enabled:
+            if self.is_mangohud_dlsym_available and \
+               self.app_id == "255710":
                 command_line.append("--dlsym")
+
+        if self.always_grab_cursor:
+            command_line.append("--force-grab-cursor")
+
+        command_line.append("--")
+
         if self.is_gamemoderun_available:
             command_line.append(self.gamemoderun_path)
 
         command_line.extend(self.args)
-    
-        print("gamescope is available:", self.is_gamescope_available)
-        print("mangohud is available:", self.is_mangohud_available)
+
+        print("Gamescope is available:", self.is_gamescope_available)
+        print("MangoHud is available:", self.is_mangohud_available)
+        print("MangoApp is available:", self.is_mangoapp_available)
         print("gamemoderun is available:", self.is_gamemoderun_available)
-        print("MangoHud dlsym enabled:", self.is_mangohud_dlsym_enabled)
+        print("Wayland is available:", self.is_wayland_available)
+        print("MangoHud dlsym enabled:", self.is_mangohud_dlsym_available)
+        print("Always grab cursor:", self.always_grab_cursor)
+        print("Launch in fullscreen:", self.fullscreen_mode)
         print("Platform:", self.CURRENT_PLATFORM)
+        print("Refresh rate:", self.refresh_rate)
         print("Running as:", self.CURRENT_USER)
         print("Running command:", ' '.join(command_line))
 
