5d71ff2853ba8da90f707f37b419600518663e57
Felipe González Martín <158048821+felgmar@users.noreply.github.com>
2025-11-13 18:51:43 +0000

    lib/event_handler: refactor event_handler imports and variable naming
    Moved ArgumentParser import to lib.args and updated variable names to uppercase for SecureBootManager and PackageManager instances in the 'setup-secure-boot' action. Improved platform validation error handling and formatted docstrings for clarity.

--- lib/event_handler.py
+++ lib/event_handler.py
@@ -8,12 +8,12 @@ if __name__ == "__main__":
 import sys
 from argparse import ArgumentError
 
+from lib.args import ArgumentParser
 from modules.kernels import KernelManager
 from modules.packages import PackageManager
 from modules.rootfs import RootFSManager
 from modules.secure_boot import SecureBootManager
 from modules.services import ServicesManager
-from modules.args import ArgumentParser
 
 ARGUMENTS_PARSER = ArgumentParser()
 ARGUMENTS_PARSER.populate_args()
@@ -29,29 +29,34 @@ def __validate_platform() -> None:
     """
     Validate the current platform and raise an error if it is not supported.
     """
-    assert __get_current_platform() == "linux", __get_current_platform() + \
-        " is not a supported platform."
+    ERROR_MESSAGE: str = __get_current_platform() + " is not a supported platform."
+    assert __get_current_platform() == "linux", ERROR_MESSAGE
 
 def parse_actions() -> None:
     """
-    Parse the action specified in the command line arguments and execute the corresponding function.
+    Parse the action specified in the command line
+    arguments and execute the corresponding function.
     """
-    __validate_platform()
+    try:
+        __validate_platform()
+    except Exception as e:
+        raise e
 
     if args.verbose:
         print("[VERBOSE] Action set to:", args.action)
         print("[VERBOSE] Your platform is:", __get_current_platform())
         input("Press any key to continue.\n")
 
+
     match args.action:
         case "setup-secure-boot":
-            sbm = SecureBootManager()
-            pm = PackageManager()
-            desktop_environment: str = pm.get_desktop_environment()
+            SECURE_BOOT_MANAGER = SecureBootManager()
+            PACKAGE_MANAGER = PackageManager()
+            desktop_environment: str = PACKAGE_MANAGER.get_desktop_environment()
 
-            sbm.install_dependencies(desktop_environment, args.verbose)
-            sbm.backup_boot_files(args.verbose)
-            sbm.install_shim(args.verbose)
+            SECURE_BOOT_MANAGER.install_dependencies(desktop_environment, args.verbose)
+            SECURE_BOOT_MANAGER.backup_boot_files(args.verbose)
+            SECURE_BOOT_MANAGER.install_shim(args.verbose)
 
         case "install-tkg-kernel":
             km = KernelManager(kernel_url="https://github.com/frogging-family/linux-tkg.git",
